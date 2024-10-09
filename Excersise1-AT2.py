1d6856d5d6f01cd76c5cf010ee13a1ee838cc305
AlainBoreal <300142993@monboreal.ca>
2024-10-08 21:11:38 -0400

    Programmes 1.5

---
+++ Excersise1-AT2.py
@@ -0,0 +1,6 @@
+x = int(input("entrer un nombre pour une table multiplication: "))
+t = [0,1,2,3,4,5,6,7,8,9,10]
+
+print("voici ton table multiplication : ")
+for i in t:
+    print(x, "x", i, "=", x * i)
